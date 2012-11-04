bbcc2f03128653d9cb773a2de56619630c82e037
John Manoogian III <jm3@jm3.net>
2012-11-03 20:39:59 -0700

    switch to em-twitter gem from tweetstream!

--- Gemfile
+++ Gemfile
@@ -3,13 +3,13 @@ source :rubygems
 gem "async-rack"  # async-proof rack middleware (github.com/rkh/async-rack.git)
 gem "barista"     # coffeescript support
 gem "cramp"       # parent web framework
+gem "em-twitter"  # for streaming api
 gem "foreman"     # manage app's process table
 gem "haml"        # rendering web view
 gem "http_router" # Rack based routing  
 gem "ohm"         # redis
 gem "thin"        # Async webserver for running a cramp application
 gem "tilt"        # for passing locals to haml template
-gem "tweetstream" # for streaming api
 gem "yajl-ruby"   # ↳ tweetstream
 
 group :development do
--- harvest-tweets.rb
+++ harvest-tweets.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
 
-require "ohm"  
-require "tweetstream"  
+require "ohm"
+require "em-twitter"
 require "yajl/json_gem"
 
 if ! ENV["OAUTH_KEY"]
@@ -20,15 +20,17 @@ DEFAULT_KEYWORD = "happy"
 
 Ohm.connect( :url => ENV["REDIS_URL"] )
 
-TweetStream.configure do |config|
-  config.consumer_key       = ENV["OAUTH_KEY"]
-  config.consumer_secret    = ENV["OAUTH_SECRET"]
-  config.oauth_token        = ENV["OAUTH_ACCESS_TOKEN"]
-  config.oauth_token_secret = ENV["OAUTH_TOKEN_SECRET"]
-
-  config.auth_method = :oauth
-  config.parser      = :yajl
-end
+# configure EM-twitter
+options = {
+  :path   => '/1/statuses/filter.json',
+  :params => { :track => 'twitter' },
+  :oauth  => {
+    :consumer_key     => ENV['OAUTH_KEY'],
+    :consumer_secret  => ENV['OAUTH_SECRET'],
+    :token            => ENV['OAUTH_ACCESS_TOKEN'],
+    :token_secret     => ENV['OAUTH_TOKEN_SECRET']
+  }
+}
 
 kind  = Ohm.redis.get("cfg:track:kind")  || ARGV[0] || DEFAULT_KIND
 query = Ohm.redis.get("cfg:track:query") || ARGV[1] || DEFAULT_KEYWORD
@@ -41,35 +43,56 @@ end
 Ohm.redis.set("cfg:track:kind", kind)
 Ohm.redis.set("cfg:track:query", query)
 
-loop do
-  TweetStream::Client.new.track( query ) do |status, client|
-    # if we've received a new tracking request, immediately bail & reload
-    client.stop if Ohm.redis.get( "cfg:track:kind" ) != kind or Ohm.redis.get( "cfg:track:query" ) != query
+EM.run do
+  client = EM::Twitter::Client.connect(options)
+  # if we've received a new tracking request, immediately bail & reload
+  # client.stop if Ohm.redis.get( "cfg:track:kind" ) != kind or Ohm.redis.get( "cfg:track:query" ) != query
 
-    if Ohm.redis.scard( "tweet:#{kind}:#{query}" ) > 1000
-      Ohm.redis.del( "tweet:#{kind}:#{query}" )
-    end
+  if Ohm.redis.scard( "tweet:#{kind}:#{query}" ) > 1000
+    Ohm.redis.del( "tweet:#{kind}:#{query}" )
+  end
 
+  client.on_unauthorized do
+    puts "unauthorized"
+  end
+  client.on_forbidden do
+    puts "forbidden"
+  end
+  client.on_not_found do
+    puts "not found"
+  end
+  client.on_not_acceptable do
+    puts "not acceptable"
+  end
+  client.on_too_long do
+    puts "too long"
+  end
+  client.on_range_unacceptable do
+    puts "range unacceptable"
+  end
+  client.on_rate_limited do
+    puts "rate limited"
+  end
+
+  client.each do |raw|
+    status =  Yajl::Parser.parse(raw)
     condensed_metadata = {
-      :text                   => status.text,
-      :screen_name            => status.user.screen_name,
-      :name                   => status.user.name,
-      :location               => status.user.location,
-      :lang                   => status.user.lang,
-      :protected              => status.user.protected,
-      :id_str                 => status.id_str,
-      :in_reply_to_user_id    => status.in_reply_to_user_id,
-      :in_reply_to_status_id  => status.in_reply_to_status_id,
-      :hashtags               => status.entities.hashtags,
-      :urls                   => status.entities.urls,
-      :user_mentions          => status.entities.user_mentions,
+      :text                   => status["text"],
+      :screen_name            => status["user"]["screen_name"],
+      :name                   => status["user"]["name"],
+      :location               => status["user"]["location"],
+      :lang                   => status["user"]["lang"],
+      :protected              => status["user"]["protected"],
+      :id_str                 => status["id_str"],
+      :in_reply_to_user_id    => status["in_reply_to_user_id"],
+      :in_reply_to_status_id  => status["in_reply_to_status_id"],
+      :hashtags               => status["entities"]["hashtags"],
+      :urls                   => status["entities"]["urls"],
+      :user_mentions          => status["entities"]["user_mentions"],
     }
-
-    puts status.text
     Ohm.redis.sadd "tweet:#{kind}:#{query}", condensed_metadata.to_json
   end
 
   kind  = Ohm.redis.get("cfg:track:kind")  || DEFAULT_KIND
   query = Ohm.redis.get("cfg:track:query") || DEFAULT_KEYWORD
 end
-
